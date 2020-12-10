e076d0c31951e47b2621ea6ab8fdd34c9cc0213e
mumali-ui <frederickmumali@gmail.com>
2020-12-10 20:13:45 +0100

    more oData, Node JS and UI5 modules

--- web/resources/odataBasic/controller/App.controller.js
+++ web/resources/odataBasic/controller/App.controller.js
@@ -3,27 +3,46 @@
 sap.ui.define([
 	"opensap/odataBasic/controller/BaseController",
 	"sap/ui/model/json/JSONModel"
-], function(BaseController, JSONModel) {
+], function (BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("opensap.odataBasic.controller.App", {
 
-		logout: function(){
+		logout: function () {
 			window.location.href = "/my/logout";
 		},
-		
-		onInit: function() {
+
+		onInit: function () {
 			this.getView().addStyleClass("sapUiSizeCompact"); // make everything inside this View appear in Compact mode
 			var oConfig = this.getOwnerComponent().getModel("config");
 			var userName = oConfig.getProperty("/UserName");
 			var bpModel = this.getOwnerComponent().getModel("bpModel");
 			var oTable = this.getView().byId("bpTable");
 
-			//To-Do Bind Model to table
-			
+			function fnLoadMetadata() {
+				try {
+					oTable.setModel(bpModel);
+					oTable.setEntitySet("BusinessPartners");
+					var oMeta = bpModel.getServiceMetadata();
+					var headerFields = "";
+					for (var i = 0; i < oMeta.dataServices.schema[0].entityType[0].property.length; i++) {
+						var property = oMeta.dataServices.schema[0].entityType[0].property[i];
+						headerFields += property.name + ",";
+					}
+					oTable.setInitiallyVisibleFields(headerFields);
+				} catch (e) {
+					console.log(e.toString());
+					oDataFailed();
+				}
+			}
+			bpModel.attachMetadataLoaded(bpModel, function () {
+				fnLoadMetadata();
+			});
+			fnLoadMetadata();
+
 		},
 
-		onErrorCall: function(oError) {
+		onErrorCall: function (oError) {
 			if (oError.statusCode === 500 || oError.statusCode === 400 || oError.statusCode === "500" || oError.statusCode === "400") {
 				var errorRes = JSON.parse(oError.responseText);
 				if (!errorRes.error.innererror) {
