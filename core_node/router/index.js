44b46a55e4f2d9e2b81368197eac5e6cafbe3f22
mumali-ui <frederickmumali@gmail.com>
2020-12-11 00:38:42 +0100

    Node .js module extension

--- core_node/router/index.js
+++ core_node/router/index.js
@@ -5,6 +5,8 @@
 module.exports = (app, server) => {
 	app.use("/node", require("./routes/myNode")());
 	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
+	app.use("/node/textBundle", require("./routes/textBundle")());
+	app.use("/node/chat", require("./routes/chatServer")(server));
 
 	app.use((err, req, res, next) => {
 		console.error(JSON.stringify(err));
